ea79423692e3f9d6603e2c9e8339a1d395f633cc
egregors <egregors@pm.me>
2022-06-10 23:17:39 +0300

    add scatb deply script

---
+++ scripts/deploy_scatb.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+docker pull ghcr.io/egregors/shoppingcatbot/scbot:latest
+docker stop scatb
+docker rm -f scatb
+docker run -d --name=scatb --env-file=scatb.env ghcr.io/egregors/shoppingcatbot/scbot
